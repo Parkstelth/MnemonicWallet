62c681f2ad473c606a7691518542a1fe532c3936
이인수 <cse-insulee@iinsuui-MacBookPro.local>
2021-11-15 11:26:47 +0900

    init setting

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HD wallet' });
+  res.render('index', { title: 'Mnemonic Wallet' });
 });
 
 module.exports = router;
