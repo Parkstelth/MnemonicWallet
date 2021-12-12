0d32786c13d2f61a747cb96aa78f569a1ba2f794
Parkstelth <stelthicmaster@gmail.com>
2021-12-12 12:11:29 +0900

    create-Mnemonic-Wallet

--- routes/wallet/index.js
+++ routes/wallet/index.js
@@ -1,17 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const lightwallet = require("eth-lightwallet");
-const fs = require('fs');
+const fs = require("fs");
 
-// TODO : lightwallet 모듈을 사용하여 랜덤한 니모닉 코드를 얻습니다.
-router.post('/newMnemonic', async(req,res) => {
+router.post("/newWallet", async (req, res) => {
+  let mnemonic;
+  try {
+    mnemonic = lightwallet.keystore.generateRandomSeed();
+    let password = req.body.password;
 
-});
+    try {
+      lightwallet.keystore.createVault({
+          password: password,
+          seedPhrase: mnemonic,
+          hdPathString: "m/0'/0'/0'",
+        },
+        function (err, ks) {
+          ks.keyFromPassword(password, function (err, pwDerivedKey) {
+            ks.generateNewAddress(pwDerivedKey, 1);
 
+            let address = ks.getAddresses().toString();
+            let keystore = ks.serialize();
 
-// TODO : 니모닉 코드와 패스워드를 이용해 keystore와 address를 생성합니다.
-router.post('/newWallet', async(req, res) => {
+            fs.writeFile(`keystore(${address}).json`, keystore, function (err, data) {
+              if (err) {
+                res.json({
+                  code: 999,
+                  message: "파일 생성 실패"
+                });
+              } else {
+                res.json({
+                  code: 1,
+                  message: `파일 생성 성공 / Wallet Address : ${address}`
+                });
+              }
+            });
+          });
+        }
+      );
+    } catch (exception) {
+      console.log("NewWallet ==>>>> " + exception);
+    }
 
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
